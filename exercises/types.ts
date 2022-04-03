118514cec2058da1164d8739eb499f9c0e2113a4
JohannesFeige <johannesfeige@gmail.com>
2022-04-03 17:19:21 +0200

    update pptx

--- exercises/types.ts
+++ exercises/types.ts
@@ -1,6 +1,6 @@
 // string
 let trainingTitle;
-trainingTitle = 'TS Basics';
+trainingTitle = "TS Basics";
 
 // number
 let attendees;
@@ -12,43 +12,43 @@ prepared = false;
 
 // array
 let topics;
-topics = ['JS Types', 'TS Types'];
+topics = ["JS Types", "TS Types"];
 topics.push(100);
 
 // tuples
 let topic;
-topic = ['JS Types', 10];
+topic = ["JS Types", 10];
 // let topic: [string, number] = [10, 'JS Types'];
 
 // enums - nothing todo here
 enum Direction {
-    up,
-    down,
-    left,
-    right,
+  up,
+  down,
+  left,
+  right,
 }
 
 // functions
 function returnAttendees(attendees) {
-    return attendees;
+  return attendees;
 }
 
 // optional parameter:
 function greetMe(name) {
-    if (!name) {
-        name = 'Default Name';
-    }
-    console.log(name);
+  if (!name) {
+    name = "Default Name";
+  }
+  console.log(name);
 }
 
 // void
 function printTitle(title) {
-    console.log(title);
+  console.log(title);
 }
 
 // argument types
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 // function types
@@ -56,62 +56,54 @@ let myAdd: (val1, val2) => any;
 myAdd = add;
 
 // optionale Parameter, default Parameter
-function concat(
-    val1,
-    val2 = 'Team',
-    val3
-): string {
-    return `${val1} ${val2} ${val3 ?? ''}`;
+function concat(val1, val2 = "Team", val3): string {
+  return `${val1} ${val2} ${val3 ?? ""}`;
 }
 
 // objects
-let training
+let training;
 training = {
-    title: 'TS 101',
-    attendees: 20,
+  title: "TS 101",
+  attendees: 20,
 };
 
 // type Training = {
-    
+
 // }
 
 // complex type:
 let typeScriptTraining = {
-    title: 'TS Basics',
-    topics: ['JS Types', 'TS Types'],
-    getTopicsAmount: function(f: string): number {
-        return this.topics.length;
-    },
+  title: "TS Basics",
+  topics: ["JS Types", "TS Types"],
+  getTopicsAmount: function (f: string): number {
+    return this.topics.length;
+  },
 };
 
 let angularTraining = {
-    title: 'ng Basics',
-    topics: ['Components', 'ng Testing'],
-    getTopicsAmount: function(f: string): number {
-        return this.topics.length;
-    },
+  title: "ng Basics",
+  topics: ["Components", "ng Testing"],
+  getTopicsAmount: function (f: string): number {
+    return this.topics.length;
+  },
 };
 
 // unknown + check types
 function convertUnkown(val) {
-    if (typeof val === 'string') {
-        return val;
-    }
-    return val.toString();
+  if (typeof val === "string") {
+    return val;
+  }
+  return val.toString();
 }
 
 // -----------------------------------------------------------------------------
 
 // union types
-type Bird = {
+type Bird = {};
 
-}
+type Fish = {};
 
-type Fish = {
-
-}
-
-function getAnimal() {};
+function getAnimal() {}
 
 // intersection types
 
@@ -123,13 +115,22 @@ function getAnimal() {};
 
 // Omit
 
+// -----------------------------------------------------------------------------
+
+// optional chaining
+
+// nullish coalescing
+
+
 // map & reduce
 
 const bankAccounts = [
-    { name: 'John', balance: 4000 },
-    { name: 'Jane', balance: 3000 },
-    { name: 'Jane', balance: 3000 },
-    { name: 'Peter', balance: -500 },
-    { name: 'Chloe', balance: 11000 },
-    { name: 'Alan', balance: NaN },
-]
+  { name: "John", balance: 4000 },
+  { name: "Jane", balance: 3000 },
+  { name: "Jane", balance: 3000 },
+  { name: "Peter", balance: -500 },
+  { name: "Chloe", balance: 11000 },
+  { name: "Alan", balance: NaN },
+];
+
+// promises
